2c5c1faabbb1ac26ef2199f7df9a7294666bdfb6
Group-9-CND <84366156+Group-9-CND@users.noreply.github.com>
2021-05-20 01:34:47 +0530

    Update UserMSApplication.java

--- src/main/java/com/infy/oms/UserMSApplication.java
+++ src/main/java/com/infy/oms/UserMSApplication.java
@@ -2,11 +2,11 @@ package com.infy.oms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
+
 public class UserMSApplication {
 	public static void main(String args[])
 	{
